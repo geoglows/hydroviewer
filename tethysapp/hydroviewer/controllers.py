7b005c2e79737a90d2d22a90a9d0f130a61628f3
Riley Hales <rileyhales1@gmail.com>
2023-11-24 22:57:42 -0700

    historical charts and styling updates

--- tethysapp/hydroviewer/controllers.py
+++ tethysapp/hydroviewer/controllers.py
@@ -1,7 +1,12 @@
 import geoglows
+import matplotlib
+import pandas as pd
 from django.http import JsonResponse
 from django.shortcuts import render
 from tethys_sdk.routing import controller
+
+# necessary for matplotlib to work in django processes
+matplotlib.use('Agg')
 
 
 @controller
@@ -20,21 +25,24 @@ def get_forecast(request):
 
 @controller(name='get-retrospective', url='get-retrospective')
 def get_retrospective(request):
-    reach_id = request.GET['reach_id']
+    reach_id = int(request.GET['reach_id'])
+
     df = geoglows.data.retrospective(reach_id)
-    # todo send json of df and averaged dfs
-    # todo implement plots in js
+    dayavg_df = geoglows.analyze.daily_averages(df)
+    monavg_df = geoglows.analyze.monthly_averages(df)
+    annavg_df = geoglows.analyze.annual_averages(df)
+
     json_response = {
-        'retro': geoglows.plots.retrospective(df, plot_type='plotly_html'),
-        'dailyAvg': '',
-        'monthlyAvg': '',
-        'annualAvg': '',
-        'fdc': '',
+        'retro': geoglows.plots.retrospective(df, plot_type='html'),
+        'dayAvg': geoglows.plots.daily_averages(dayavg_df, plot_type='html'),
+        'monAvg': geoglows.plots.monthly_averages(monavg_df, plot_type='html'),
+        'annAvg': geoglows.plots.annual_averages(annavg_df, plot_type='html'),
     }
+
     return JsonResponse(json_response)
 
 
 @controller(name='find-river', url='find-river')
 def find_river(request):
-    lat, lon = geoglows.streams.reach_to_latlon(request.GET['reach_id'])
+    lat, lon = geoglows.streams.reach_to_latlon(int(request.GET['reach_id']))
     return JsonResponse({'lat': lat, 'lon': lon})
--- tethysapp/hydroviewer/public/js/geoglows_hydroviewer_core.js
+++ tethysapp/hydroviewer/public/js/geoglows_hydroviewer_core.js
@@ -1,11 +1,18 @@
 const app = (() => {
     'use strict'
+
+//////////////////////////////////////////////////////////////////////// State Variables
     ////// URLS FROM TEMPLATE RENDER
     // const endpoint = "{{ endpoint }}";
     // const URL_getForecastData = "{% url 'hydroviewer:get-forecast' %}";
     // const URL_getHistoricalData = "{% url 'hydroviewer:get-retrospective' %}";
     // const URL_findReachID = "{% url 'hydroviewer:find-river' %}";
 
+    let loadingStatus = {reachid: "clear", forecast: "clear", retro: "clear"}
+    let REACHID
+    let mapMarker = null
+
+//////////////////////////////////////////////////////////////////////// ESRI Map
     const mapObj = L.map("map", {
         zoom: 3,
         minZoom: 2,
@@ -13,11 +20,7 @@ const app = (() => {
         maxBounds: L.latLngBounds(L.latLng(-100, -225), L.latLng(100, 225)),
         center: [20, 0]
     })
-
-    let REACHID
-    let mapMarker = null
     let selectedSegment = L.geoJSON(false, {weight: 5, color: "#00008b"}).addTo(mapObj)
-
     const basemapsJson = {
         "ESRI Topographic": L.esri.basemapLayer("Topographic").addTo(mapObj),
         "ESRI Terrain": L.layerGroup([
@@ -46,20 +49,18 @@ const app = (() => {
     mapObj.getPane("viirs").style.zIndex = 200
     // add the legends box to the map
     let legend = L.control({position: "bottomright"})
-    legend.onAdd = function () {
+    legend.onAdd = () => {
         let div = L.DomUtil.create("div", "legend")
-        let start =
-            '<div><svg width="20" height="20" viewPort="0 0 20 20" xmlns="http://www.w3.org/2000/svg">'
+        const legendEntries = [
+            ["purple", "20+ yr Return Period Flow"],
+            ["red", "10+ yr Return Period Flow"],
+            ["gold", "2+ yr Return Period Flow"],
+            ["blue", "Stream Line"]
+        ]
+        const polyLineSVG = (color, label) => `<div><svg width="20" height="20" viewPort="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><polyline points="19 1, 1 6, 19 14, 1 19" stroke="${color}" fill="transparent" stroke-width="2"/></svg>${label}</div>`
         div.innerHTML =
             '<div class="legend">' +
-            start +
-            '<polyline points="19 1, 1 6, 19 14, 1 19" stroke="purple" fill="transparent" stroke-width="2"/></svg> 20-yr Return Period Flow </div>' +
-            start +
-            '<polyline points="19 1, 1 6, 19 14, 1 19" stroke="red" fill="transparent" stroke-width="2"/></svg> 10-yr Return Period Flow </div>' +
-            start +
-            '<polyline points="19 1, 1 6, 19 14, 1 19" stroke="gold" fill="transparent" stroke-width="2"/></svg> 2-yr Return Period Flow</div>' +
-            start +
-            '<polyline points="19 1, 1 6, 19 14, 1 19" stroke="blue" fill="transparent" stroke-width="2"/></svg> Stream Line </div>' +
+            legendEntries.map(entry => polyLineSVG(...entry)).join("").join("") +
             "</div>"
         return div
     }
@@ -80,26 +81,25 @@ const app = (() => {
     layerAnimationTime.setUTCMinutes(0)
     layerAnimationTime.setUTCSeconds(0)
     layerAnimationTime.setUTCMilliseconds(0)
-    const/**/ currentDate = $("#current-map-date")
+    const currentDate = $("#current-map-date")
+    const slider = $("#time-slider")
     const animationDays = 5
     const stepsPerDay = 8
     const numAnimateSteps = animationDays * stepsPerDay
     const startDateTime = new Date(layerAnimationTime)
     const endDateTime = new Date(layerAnimationTime.setUTCHours(animationDays * 24))
+    let animate = false
+    let animateSpeed = 750
     layerAnimationTime = new Date(startDateTime)
     currentDate.html(layerAnimationTime)
-    const slider = $("#time-slider")
     slider.change(_ => refreshLayerAnimation())
 
-    function refreshLayerAnimation() {
+    const refreshLayerAnimation = () => {
         layerAnimationTime = new Date(startDateTime)
         layerAnimationTime.setUTCHours(slider.val() * 3)
         currentDate.html(layerAnimationTime)
         esriStreamLayer.setTimeRange(layerAnimationTime, endDateTime)
     }
-
-    let animate = false
-    let animateSpeed = 750
     const playAnimation = (once = false) => {
         if (!animate) return
         animate = !once  // toggle animation if play once (once =true, animate = false)
@@ -151,7 +151,7 @@ const app = (() => {
             mapObj.removeLayer(mapMarker)
         }
         mapMarker = L.marker(event.latlng).addTo(mapObj)
-        updateStatusIcons("identify")
+        updateStatusIcons({reachid: "load", forecast: "clear", retro: "clear"})
         $("#chart_modal").modal("show")
 
         L.esri
@@ -162,16 +162,15 @@ const app = (() => {
             .precision(3) // decimals in the returned coordinate pairs
             .run((error, featureCollection) => {
                 if (error) {
-                    updateStatusIcons("fail")
+                    updateStatusIcons({reachid: "fail"})
                     alert("Error finding the reach_id")
                     return
                 }
+                updateStatusIcons({reachid: "ready"})
                 selectedSegment.clearLayers()
                 selectedSegment.addData(featureCollection.features[0].geometry)
                 REACHID = featureCollection.features[0].properties["COMID (Stream Identifier)"]
                 clearChartDivs()
-                updateStatusIcons("load")
-                updateDownloadLinks("clear")
                 getForecastData()
                 getHistoricalData()
             })
@@ -190,13 +189,11 @@ const app = (() => {
                 mapMarker = L.marker(L.latLng(response.lat, response.lon)).addTo(mapObj)
                 mapObj.flyTo(L.latLng(response["lat"], response["lon"]), 9)
             },
-            error: () => {
-                alert("Unable to find that reach_id. Please check the number and try again.")
-            }
+            error: () => alert("Unable to find that reach_id. Please check the number and try again.")
         })
     }
 
-    function findLatLon() {
+    const findLatLon = () => {
         let ll = prompt(
             "Enter a latitude and longitude coordinate pair separated by a comma. For example, to get to BYU you would enter: 40.25, -111.65",
             "40.25, -111.65"
@@ -212,7 +209,7 @@ const app = (() => {
     }
 
 //////////////////////////////////////////////////////////////////////// UPDATE DOWNLOAD LINKS FUNCTION
-    function updateDownloadLinks(type) {
+    const updateDownloadLinks = type => {
         if (type === "clear") {
             $("#download-forecast-btn").attr("href", "")
             $("#download-historical-btn").attr("href", "")
@@ -223,26 +220,23 @@ const app = (() => {
     }
 
 ////////////////////////////////////////////////////////////////////////  GET DATA FROM API AND MANAGING PLOTS
-    const chart_divs = [
-        $("#forecast-chart"),
-        $("#forecast-table"),
-        $("#historical-chart"),
-        $("#historical-table"),
-        $("#daily-avg-chart"),
-        $("#monthly-avg-chart"),
-        $("#flowduration-chart"),
-        $("#volume_plot"),
-        $("#scatters"),
-        $("#stats_table")
+    const chartDivs = [
+        $("#forecastPlot"),
+        $("#fcEnsPlot"),
+        $("#fcProbTable"),
+        $("#retroPlot"),
+        $("#dayAvgPlot"),
+        $("#monAvgPlot"),
+        $("#annAvgPlot"),
     ]
 
-    function getForecastData() {
+    const getForecastData = () => {
         let ftl = $("#forecast_tab_link") // select divs with jquery so we can reuse them
         ftl.tab("show")
-        let fc = chart_divs[0]
+        let fc = chartDivs[0]
         fc.html(`<img alt="loading signal" src=${loading_gif}>`)
         fc.css("text-align", "center")
-        updateStatusIcons({forecast: "loading"})
+        updateStatusIcons({forecast: "load"})
         $.ajax({
             type: "GET",
             async: true,
@@ -261,53 +255,61 @@ const app = (() => {
         })
     }
 
-    function getHistoricalData() {
-        updateStatusIcons({"retro": "loading"})
+    const getHistoricalData = () => {
+        updateStatusIcons({retro: "load"})
         updateDownloadLinks("clear")
         let tl = $("#historical_tab_link") // select divs with jquery so we can reuse them
         tl.tab("show")
-        let plotdiv = chart_divs[2]
+        let plotdiv = chartDivs[2]
         plotdiv.css("text-align", "center")
         $.ajax({
             type: "GET",
             async: true,
             data: {reach_id: REACHID},
             url: URL_getHistoricalData,
-            success: function (response) {
-                // historical tab
+            success: response => {
+                updateStatusIcons({retro: "ready"})
                 tl.tab("show")
-                $("#get-historical-btn").hide()
-                plotdiv.html(response.plotRetro)
-                updateStatusIcons({"retro": "ready"})
+                plotdiv.html(response.retro)
+                $("#dayAvgPlot").html(response.dayAvg)
+                $("#monAvgPlot").html(response.monAvg)
+                $("#annAvgPlot").html(response.annAvg)
             },
-            error: function () {
-                updateStatusIcons({"retro": "fail"})
-            }
+            error: () => updateStatusIcons({retro: "fail"})
         })
     }
 
 //////////////////////////////////////////////////////////////////////// UPDATE STATUS ICONS FUNCTION
-    function updateStatusIcons(type) {
-        let statusObj = $("#request-status")
-        if (type === "identify") {
-            statusObj.html(" (Getting Stream ID)")
-            statusObj.css("color", "orange")
-        } else if (type === "load") {
-            statusObj.html(" (Loading ID " + REACHID + ")")
-            statusObj.css("color", "orange")
-        } else if (type === "ready") {
-            statusObj.html(" (Ready)")
-            statusObj.css("color", "green")
-        } else if (type === "fail") {
-            statusObj.html(" (Failed)")
-            statusObj.css("color", "red")
-        } else if (type === "cleared") {
-            statusObj.html(" (Cleared)")
-            statusObj.css("color", "grey")
+    const updateStatusIcons = status => {
+        for (let key in status) {
+            loadingStatus[key] = status[key]
         }
+        let statusDivs = [
+            ['reachid', 'Find River ID'],
+            ['forecast', 'Get Forecast'],
+            ['retro', 'Get Historical']
+        ].map(key => {
+            let message
+            switch (loadingStatus[key[0]]) {
+                case "load":
+                    message = key[0] === "reachid" ? "Identifying" : "Loading"
+                    break
+                case "ready":
+                    message = key[0] === "reachid" ? REACHID : "Ready"
+                    break
+                case "fail":
+                    message = "Failed"
+                    break
+                case "clear":
+                    message = "none"
+            }
+            return `<span class="status-${loadingStatus[key[0]]}">${key[1]}: ${message}</span>`
+        }).join(' - ')
+
+        $("#request-status").html(statusDivs)
     }
 
-    function fix_buttons(tab) {
+    const fix_buttons = tab => {
         let buttons = [
             $("#download-forecast-btn"),
             $("#download-historical-btn"),
@@ -324,37 +326,31 @@ const app = (() => {
         $("#resize_charts").attr({onclick: "fixChartSizes('" + tab + "')"})
     }
 
-    function fixChartSizes(tab) {
-        let divs = []
-        if (tab === "forecast") {
-            divs = [$("#forecast-chart .js-plotly-plot")]
-        } else if (tab === "historical") {
-            divs = [$("#historical-5-chart .js-plotly-plot")]
-        }
-        for (let i in divs) {
+    const fixChartSizes = tab => {
+        //     divs = [$("#forecastPlot .js-plotly-plot")]
+        const divsToFix = tab === "forecast" ? chartDivs.slice(0, 3) : chartDivs.slice(3)
+        divsToFix.forEach(div => {
+            // select divs with the .js-plotly-plot class
             try {
-                divs[i].css("height", 500)
-                Plotly.Plots.resize(divs[i][0])
+                div.css("height", "500px")
+                Plotly.Plots.resize(div[0])
             } catch (e) {
             }
-        }
+        })
     }
 
-    function clearChartDivs() {
-        for (let i in chart_divs) {
-            chart_divs[i].html("")
-        }
+    const clearChartDivs = () => {
+        chartDivs.forEach(div => {
+            div.css("height", "0px")
+            div.html("")
+        })
     }
 
-    $("#forecast_tab_link").on("click", function () {
-        fix_buttons("forecast")
-    })
-    $("#historical_tab_link").on("click", function () {
-        fix_buttons("historical")
-    })
+    $("#forecast_tab_link").on("click", () => fix_buttons("forecast"))
+    $("#historical_tab_link").on("click", () => fix_buttons("historical"))
 
     const clearMarkers = () => {
-        if (mapMarker) {mapObj.removeLayer(mapMarker)}
+        if (mapMarker) mapObj.removeLayer(mapMarker)
     }
     return {
         fixChartSizes,
