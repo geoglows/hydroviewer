4a2d251b63fd182bab5e2b89f2e5a5e022ecc6e0
samoldh <146383989+samoldh@users.noreply.github.com>
2024-08-13 16:05:56 -0600

    Added Swedish language support (#3)

---
+++ static/js/localization.sv.js
@@ -0,0 +1,45 @@
+window.text = {
+  "words": {
+    "riverid": "Flod-ID",
+    "forecast": "Prognos",
+    "retro": "Historisk",
+    "year": "år",
+    "years": "år",
+    "all": "Alla"
+  },
+  "prompts": {
+    "mobile": "Svep för att flytta. Nyp för att zooma. Tryck för att välja floder. Svep det här meddelandet för att dölja.",
+    "tryRiverAgain": "Flod inte hittad. Försök att zooma in och klicka exakt på vattendraget.",
+    "enterRiverID": "Ange ett giltigt 9-siffrigt flod-ID.",
+    "invalidRiverID": "Fel: Flod-ID måste vara ett 9-siffrigt tal.",
+    "findingRiver": "Identifierar flodsegment. Grafer kommer att laddas snart."
+  },
+  "status": {
+    "load": "Laddar",
+    "ready": "Klar",
+    "fail": "Misslyckades",
+    "clear": "ingen",
+    "unknown": "Okänt flodnamn"
+  },
+  "tooltips": {
+    "home": "Gå till Start",
+    "basemap": "Byt Baskarta",
+    "filter": "Filtrera Data",
+    "scale": "Skalstreck",
+    "legend": "Förklaring"
+  },
+  "inputs": {
+    "forecast": "Visa Prognosgrafer",
+    "retro": "Visa Historiska Grafer"
+  },
+  "plots": {
+    "fcTitle": "Flodprognos för",
+    "fcXaxis": "Datum",
+    "fcYaxis": "Flöde",
+    "fcLineUncertainty": "Osäkerhet",
+    "fcLineMedian": "Förutspått Flöde",
+    "retroTitle": "Historiskt Flöde för",
+    "retroXaxis": "Datum",
+    "retroYaxis": "Flöde"
+  }
+}
