635f598a76a92738367827bef4e0c3250f158878
rhuber <rachel.shayla.huber.55@gmail.com>
2024-08-13 11:40:20 -0600

    french added

---
+++ static/js/localization.fr.js
@@ -0,0 +1,45 @@
+window.text = {
+  "words": {
+    "riverid": "ID de la Rivière",
+    "forecast": "Prévision",
+    "retro": "Rétrospective",
+    "year": "année",
+    "years": "ans",
+    "all": "tout"
+  },
+  "prompts": {
+    "mobile": "Glissez pour vous déplacer. Pincez pour zoomer. Appuyez pour sélectionner des rivières. Faites glisser ce message pour le ignorer.",
+    "tryRiverAgain": "Rivière introuvable. Essayez d'être proche et précis lorsque vous cliquez sur le flux.",
+    "enterRiverID": "Veuillez saisir un identifiant Rio valide à 9 chiffres.",
+    "invalidRiverID": "Erreur : les numéros d'identification Rio doivent être des numéros à 9 chiffres.",
+    "findingRiver": "Identifier un segment de rivière. Les graphiques seront bientôt téléchargés."
+  },
+  "status": {
+    "load": "Chargement",
+    "ready": "Prêt",
+    "fail": "Échoué",
+    "clear": "aucun",
+    "unknown": "Nom de la rivière inconnu"
+  },
+  "tooltips": {
+    "home": "Aller à la Maison",
+    "basemap": "Modifier la carte de base",
+    "filter": "Filtrer les Données",
+    "scale": "Barre d'échelle",
+    "legend": "Légende"
+  },
+  "inputs": {
+    "forecast": "Afficher les Graphiques de Prévision",
+    "retro": "Afficher les Graphiques Historiques"
+  },
+  "plots": {
+    "fcTitle": "Prévisions fluviales pour",
+    "fcXaxis": "Date",
+    "fcYaxis": "Débit",
+    "fcLineUncertainty": "Incertitude",
+    "fcLineMedian": "Débit prévu",
+    "retroTitle": "Débit Historique pour",
+    "retroXaxis": "Date",
+    "retroYaxis": "Débit"
+  }
+}
